5b473aca72f571725986bbe9d563a32f9ad75aba
Suraj <mauryasuraj6266444@gmail.com>
2022-01-04 19:24:27 +0530

    Added another page and modified some file

--- js/script.js
+++ js/script.js
@@ -1,8 +1,10 @@
 let menuIcon = document.querySelector(".menu-icon");
 let sideBar = document.querySelector(".sidebar");
 let container = document.querySelector(".container");
+let subscribe = document.querySelector(".subscribe-list")
 
 menuIcon.onclick = function() {
     sideBar.classList.toggle("small-sidebar")
     container.classList.toggle("large-container")
+    subscribe.classList.toggle("subscribe-none")
 }
